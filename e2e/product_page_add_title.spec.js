4a57ca5a4400752409a65413149fe536ab5209bb
sayhitosujith <sayhitosujith@gmail.com>
2025-04-01 22:53:00 +0530

    added assert

--- e2e/product_page_add_title.spec.js
+++ e2e/product_page_add_title.spec.js
@@ -5,10 +5,16 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:2221/');
 
   const addToBasketButton = page.locator('[data-qa="product-button"]').first()
+  const basketCounter = page.locator('[data-qa="header-basket-count"]')
+
   await addToBasketButton.waitFor()
+  await expect(addToBasketButton).toHaveText("Add to Basket")
+  await expect(basketCounter).toHaveText("0")
+
   await addToBasketButton.click()
 
+  await expect(addToBasketButton).toHaveText("Remove from Basket")
+  await expect(basketCounter).toHaveText("1")
 
-  expect(addToBasketButton).toHaveText("Remove from Basket")
-});
+})
 
