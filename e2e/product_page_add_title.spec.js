9ddb0664a1883a5509c2c093b86c57db3b54e586
sayhitosujith <sayhitosujith@gmail.com>
2025-04-01 22:36:25 +0530

    adding assert

--- e2e/product_page_add_title.spec.js
+++ e2e/product_page_add_title.spec.js
@@ -4,7 +4,11 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:2221/');
 
-  const addToBasketButton = page.locator('div').filter({ hasText: /^499\$Add to Basket$/ }).getByRole('button')
- await addToBasketButton.click()
+  const addToBasketButton = page.locator('[data-qa="product-button"]').first()
+  await addToBasketButton.waitFor()
+  await addToBasketButton.click()
+
+
+  expect(addToBasketButton).toHaveText("Remove from Basket")
 });
 
