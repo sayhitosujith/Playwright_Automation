61f8dba4c542420840d30dce99567439ab63be88
sayhitosujith <sayhitosujith@gmail.com>
2025-04-03 18:29:16 +0530

    vdzb

--- e2e/product_page_add_title.spec.js
+++ e2e/product_page_add_title.spec.js
@@ -2,10 +2,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.naukri.com/nlogin/login?utm_source=google&utm_medium=cpc&utm_campaign=Brand&gad_source=1&gclid=CjwKCAjwo6GyBhBwEiwAzQTmc34DfBd9dNPPn_R_W3UozmHxoGFxQRepNJgOcFPHLMUoYhQ2rGv9nBoC5ZYQAvD_BwE', { waitUntil: 'load' });
+  await page.goto('https://www.naukri.com/nlogin/login?utm_source=google&utm_medium=cpc&utm_campaign=Brand&gad_source=1&gclid=CjwKCAjwo6GyBhBwEiwAzQTmc34DfBd9dNPPn_R_W3UozmHxoGFxQRepNJgOcFPHLMUoYh[...]');
 
-  const UserName = page.locator("//input[@id='usernameField']");
-  const Password = page.locator("//input[@id='passwordField']");
+  const UserName = page.locator('[id="usernameField"]');
+  const Password = page.locator('[id="passwordField"]');
   const LoginButton = page.locator("//button[@class='waves-effect waves-light btn-large btn-block btn-bold blue-btn textTransform']");
   const viewProfile = page.locator("//a[normalize-space()='View profile']");
   const fileInput = page.locator('[class="dummyUpload typ-14Bold"]');
@@ -13,19 +13,15 @@ test('has title', async ({ page }) => {
   const ClearText = page.locator("//textarea[@id='resumeHeadlineTxt']");
   const SaveButton = page.locator("//button[normalize-space()='Save']");
 
-  // Increase timeout for filling elements
-  await UserName.fill("sayhitosujith@gmail.com", { timeout: 60000 });
-  await Password.fill("Qw@12345678", { timeout: 60000 });
-  
-  // Ensure the login button is visible before clicking
-  await LoginButton.waitFor({ state: 'visible', timeout: 60000 });
+  // Increase timeout to 60 seconds
+  await UserName.waitFor({ state: 'visible', timeout: 60000 });
+  await UserName.fill("sayhitosujith@gmail.com");
+
+  await Password.fill("Qw@12345678");
   await LoginButton.click();
+  await page.waitForTimeout(5000);
 
-  // Wait for profile to be visible before clicking
-  await viewProfile.waitFor({ state: 'visible', timeout: 60000 });
   await viewProfile.click();
-
-  // Add additional waits if necessary
   await page.waitForTimeout(5000);
   await page.mouse.wheel(0, 500); // Scroll down 500 pixels
 
