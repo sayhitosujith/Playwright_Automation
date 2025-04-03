429cb1c9a0f054dc4a2b906795e23bbb0585e41c
sayhitosujith <sayhitosujith@gmail.com>
2025-04-03 15:21:51 +0530

    added wait for

--- e2e/product_page_add_title.spec.js
+++ e2e/product_page_add_title.spec.js
@@ -5,9 +5,9 @@ test('Login Test', async ({ page }) => {
 
   await page.waitForSelector('//input[@id="usernameField"]', { timeout: 60000 }); // Ensure field exists
 
-  const UserName = page.locator('//input[@id="usernameField"]');
-  const Password = page.locator('//input[@id="passwordField"]');
-  const LoginButton = page.locator("//button[@class='waves-effect waves-light btn-large btn-block btn-bold blue-btn textTransform']");
+  const UserName = page.locator('//input[@id="usernameField"]').waitFor({ state: 'visible', timeout: 60000 });;
+  const Password = page.locator('//input[@id="passwordField"]').waitFor({ state: 'visible', timeout: 60000 });;
+  const LoginButton = page.locator("//button[@class='waves-effect waves-light btn-large btn-block btn-bold blue-btn textTransform']").waitFor({ state: 'visible', timeout: 60000 });;
 
   await UserName.fill("sayhitosujith@gmail.com");
 
