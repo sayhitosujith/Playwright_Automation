935492fa53254f5ca2e0e5864ef4a91ae4bea7cd
sayhitosujith <sayhitosujith@gmail.com>
2025-04-07 22:17:14 +0530

    40000

--- e2e/product_page_add_title.spec.js
+++ e2e/product_page_add_title.spec.js
@@ -11,7 +11,7 @@ test('has title', async ({ page }) => {
   const ClearText = page.locator("//textarea[@id='resumeHeadlineTxt']");
   const SaveButton = page.locator("//button[normalize-space()='Save']");
 
-  await UserName.fill("sayhitosujith@gmail.com", { timeout: 30000 });
+  await UserName.fill("sayhitosujith@gmail.com", { timeout:40000 });
   await Password.fill("Qw@12345678");
   await LoginButton.click();
 
